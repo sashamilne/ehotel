995b7637f536f3c9052c769a15d8b877b4a43fcf
Sasha Milne <147281806+sashamilne@users.noreply.github.com>
2025-03-28 22:27:54 -0400

    testing apache tomcat and maven

---
+++ ehotel/src/main/java/com/example/MainServlet.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")  // This makes it the main servlet
+public class MainServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to My Maven + Tomcat App!</h1>");
+    }
+}
